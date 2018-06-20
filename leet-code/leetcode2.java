1f4b415db07fb21e1ca1fb5e60aef3375e294d0d
lgxisbb@164.com <xiaoqiang321>
2018-06-20 21:55:06 +0800

    刷了两题

---
+++ leet-code/leetcode2.java
@@ -0,0 +1,45 @@
+import java.awt.List;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ * 给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        String s1 = "";
+        String s2 = "";
+
+        while (null != l1 ) {
+            s1 = l1.val + s1;
+            l1 = l1.next;
+        }
+
+        while (null != l2) {
+            s2 = l2.val + s2;
+            l2 = l2.next;
+        }
+        String sum = new BigInteger(s1).add(new BigInteger(s2)).toString();
+        char[] sumchar = sum.toCharArray();
+        ListNode res = new ListNode(0);
+        for (char cnum : sumchar) {
+            ListNode newNode = new ListNode(Integer.valueOf(String.valueOf(cnum)));
+            newNode.next = res.next;
+            res.next = newNode;
+        }
+        return res.next;
+    }
+
+    
+}
